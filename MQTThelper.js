6d4882d8f531a202bbe9f844dafcc26cd2ee1ee3
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-06-05 20:30:10 +0700

    Update MQTThelper.js

--- MQTThelper.js
+++ MQTThelper.js
@@ -1,67 +1,66 @@
 <script src="paho-mqtt.js"></script>
-<script>
-  const arrayTopics = [
-    "lygiahuy05022002/feeds/temp",
-    "lygiahuy05022002/feeds/humi",
-    "lygiahuy05022002/feeds/light",
-    "lygiahuy05022002/feeds/relay1",
-    "lygiahuy05022002/feeds/relay2",
-    "lygiahuy05022002/feeds/relay3",
-    "lygiahuy05022002/feeds/ledbutton",
-    "lygiahuy05022002/feeds/doorbutton",
-    "lygiahuy05022002/feeds/security"
-  ];
 
-  const clientId = "12345678";
-  const username = "lygiahuy05022002";
-  const password = "aio" + "_kHoz82rC78CkW8BT3G9jf3LXAoDy";
+const arrayTopics = [
+  "lygiahuy05022002/feeds/temp",
+  "lygiahuy05022002/feeds/humi",
+  "lygiahuy05022002/feeds/light",
+  "lygiahuy05022002/feeds/relay1",
+  "lygiahuy05022002/feeds/relay2",
+  "lygiahuy05022002/feeds/relay3",
+  "lygiahuy05022002/feeds/ledbutton",
+  "lygiahuy05022002/feeds/doorbutton",
+  "lygiahuy05022002/feeds/security"
+];
 
-  const serverUri = "mqtt://io.adafruit.com:1883";
+const clientId = "12345678";
+const username = "lygiahuy05022002";
+const password = "aio" + "_kHoz82rC78CkW8BT3G9jf3LXAoDy";
 
-  const mqttClient = new Paho.MQTT.Client(serverUri, clientId);
+const serverUri = "mqtt://io.adafruit.com:1883";
 
-  mqttClient.onConnectionLost = onConnectionLost;
-  mqttClient.onMessageArrived = onMessageArrived;
+const mqttClient = new Paho.MQTT.Client(serverUri, clientId);
 
-  const connectOptions = {
-    onSuccess: onConnect,
-    onFailure: onFailure,
-    userName: username,
-    password: password,
-    useSSL: true
-  };
+mqttClient.onConnectionLost = onConnectionLost;
+mqttClient.onMessageArrived = onMessageArrived;
 
-  mqttClient.connect(connectOptions);
+const connectOptions = {
+  onSuccess: onConnect,
+  onFailure: onFailure,
+  userName: username,
+  password: password,
+  useSSL: true
+};
 
-  function onConnect() {
-    console.log('Ket noi thanh cong ...');
-    subscribeToTopics();
+mqttClient.connect(connectOptions);
+
+function onConnect() {
+  console.log('Ket noi thanh cong ...');
+  subscribeToTopics();
+}
+
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.error('Ket noi bi mat:', responseObject.errorMessage);
   }
+}
 
-  function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-      console.error('Ket noi bi mat:', responseObject.errorMessage);
-    }
-  }
+function onMessageArrived(message) {
+  console.log('Nhan du lieu:', message.payloadString, ', feed id:', message.destinationName.split('/').pop());
+}
 
-  function onMessageArrived(message) {
-    console.log('Nhan du lieu:', message.payloadString, ', feed id:', message.destinationName.split('/').pop());
-  }
+function onFailure(responseObject) {
+  console.error('Loi ket noi:', responseObject.errorMessage);
+}
 
-  function onFailure(responseObject) {
-    console.error('Loi ket noi:', responseObject.errorMessage);
-  }
-
-  function subscribeToTopics() {
-    arrayTopics.forEach((topic) => {
-      mqttClient.subscribe(topic, {
-        onSuccess: function () {
-          console.log('Da subscribe thanh cong cho topic:', topic);
-        },
-        onFailure: function (err) {
-          console.error('Loi khi subscribe:', err.errorMessage);
-        }
-      });
+function subscribeToTopics() {
+  arrayTopics.forEach((topic) => {
+    mqttClient.subscribe(topic, {
+      onSuccess: function () {
+        console.log('Da subscribe thanh cong cho topic:', topic);
+      },
+      onFailure: function (err) {
+        console.error('Loi khi subscribe:', err.errorMessage);
+      }
     });
-  }
-</script>
+  });
+}
