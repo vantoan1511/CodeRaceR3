41d14a8de6c804543bdfb4f9d5d7a45e88c5ddd1
unknown <vantoan1517@gmail.com>
2023-06-05 13:54:26 +0700

    update demo.js

--- demo.js
+++ demo.js
@@ -2,9 +2,10 @@ const plugin = ({ widgets, simulator, vehicle }) => {
 
     const div = document.createElement("div")
     div.innerHTML = `
-    <div>
-    Hello World!
-    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <button type="button" class="btn btn-outline-primary">Primary</button>
+    <button type="button" class="btn btn-outline-secondary">Secondary</button>
     `
 
     widgets.register("test",
