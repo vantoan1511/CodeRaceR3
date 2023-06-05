7b24a87b4b525179f3e03b037410f33c053b64fd
unknown <vantoan1517@gmail.com>
2023-06-05 15:20:15 +0700

    add

---
+++ GGMap.js
@@ -0,0 +1,23 @@
+import GoogleMapsFromSignal from "https://playground-plugins.netlify.app/reusable/GoogleMapsFromSignal.js"
+
+const GoogleMapsPlugin = ({widgets, vehicle}) => {
+    widgets.register(
+        "GoogleMapDirections",
+        GoogleMapsFromSignal(
+            [
+                {
+                    "lat": 10.030342,
+                    "lng": 105.751816
+                },
+                {
+                    "lat": 10.030751,
+                    "lng": 105.752240
+                },
+            ],
+            vehicle,
+            { iterate: true }
+        )
+    )
+}
+
+export default GoogleMapsPlugin
