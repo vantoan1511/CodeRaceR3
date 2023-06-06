c05ee0d7dc731a1044fa865ba46bf7e02ad454c0
ngokhoadang <125115863+ngokhoadang@users.noreply.github.com>
2023-06-06 13:31:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 
 const http = require('http');
 const server = http.createServer(app);
+const {
+    Server
+} =  require('socket.io')
 
+const io = new Server(server)
 app.get('/', (req,res)=>{
-    res.send('hello world');
+   res.sendFile(__dirname+'/test.htm')
 })
 
-
+io.on('connection', (socket)=>{
+    console.log('user connected');
+})
 server.listen(3000,()=>{
     console.log('listen');
 })
