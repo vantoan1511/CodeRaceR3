733592d166369932a54184295589e56970fd06d2
unknown <vantoan1517@gmail.com>
2023-06-06 11:04:52 +0700

    update

--- MyMap.js
+++ MyMap.js
@@ -1,18 +1,30 @@
 const plugin = ({ widgets }) => {
 
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 21.0168864, lng: 105.7855574 },
-            zoom: 15
-        });
-    }
-
     const div = document.createElement("div")
     div.innerHTML = `
     
     <div id="map"></div>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDo-gybL3WcIWNMYjt0MsB9mkSN2lFci0c&callback=initMap" async defer></script>
+    <script>
+
+        function initMap() {
+            const center = { lat: 37.7749, lng: -122.4194 };
+
+            const map = new google.maps.Map(document.getElementById('map'), {
+                center: center,
+                zoom: 20
+            });
+
+            const marker = new google.maps.Marker({
+                position: center,
+                map: map,
+                title: 'Center Marker'
+            });
+        }
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDo-gybL3WcIWNMYjt0MsB9mkSN2lFci0c&callback=initMap"
+        async defer></script>
 
     `
 
