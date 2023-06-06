db19ae30c2b554e6f7ec6a39c803bf2e2ded9e70
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-06-06 14:38:48 +0700

    update plugin

---
+++ table.js
@@ -0,0 +1,17 @@
+import StatusTable from "reusable/StatusTable.js"
+
+const plugin = ({ widgets, simulator, vehicle }) => {
+
+    widgets.register("Table", StatusTable({
+        apis: ["Vehicle.Speed", "Vehicle.TripMeterReading", "Vehicle.Acceleration.Lateral", "Vehicle.Acceleration.Longitudinal", "Vehicle.Acceleration.Vertical", "Vehicle.AngularVelocity.Roll", "Vehicle.AngularVelocity.Pitch", "Vehicle.AngularVelocity.Yaw", "Vehicle.CurrentLocation.Latitude", "Vehicle.CurrentLocation.Longitude"],
+        vehicle: vehicle,
+        refresh: 1000
+    }));
+
+    let sim_function;
+    simulator("Vehicle.Speed", "subscribe", async ({ func, args }) => {
+        sim_function = args[0]
+    })
+}
+
+export default plugin;
