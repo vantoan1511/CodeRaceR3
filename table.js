d8ec2eedb56b26ecaf153d4c558e7a1481ce03c7
unknown <vantoan1517@gmail.com>
2023-06-06 20:53:11 +0700

    update

--- table.js
+++ table.js
@@ -7,26 +7,6 @@ const plugin = ({ widgets, simulator, vehicle }) => {
         vehicle: vehicle,
         refresh: 500
     }));
-
-    let sim_function;
-    simulator("Vehicle.Speed", "subscribe", async ({ func, args }) => {
-        sim_function = args[0]
-    })
-
-    // simulator("Vehicle.Speed", "get", async () => {
-    //     return (parseFloat(parseFloat(VSSdata[index]["Vehicle.Speed"]).toFixed() * 3.6).toFixed(2) + " km/h");
-    // })
-
-    return {
-        start_simulation: (time, skip = 1) => {
-            let sim_intervalId = setInterval(async () => {
-                for (let i = 0; i < skip; i++) {
-                    await vehicle.Next.get()
-                }
-                sim_function()
-            }, time)
-        }
-    }
 }
 
 export default plugin;
