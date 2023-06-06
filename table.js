518d33ec6825bf4a773eaca3265df8334ad83dcb
unknown <vantoan1517@gmail.com>
2023-06-06 22:30:27 +0700

    update nho?

--- table.js
+++ table.js
@@ -2,22 +2,6 @@ import StatusTable from "./reusable/StatusTable.js"
 
 const plugin = ({ widgets, simulator, vehicle }) => {
 
-    const init = async () => {
-        simulator("Vehicle.Speed", "get", async () => {
-            return 10
-        })
-    }
-
-    init()
-
-    const set_speed = async () => {
-        var currentSpeed = await vehicle.Speed.get()
-        simulator("Vehicle.Speed", "get", async () => {
-            return currentSpeed * 1.15
-        })
-    }
-
-
     widgets.register("Table", StatusTable({
         apis: ["Vehicle.Speed"],
         vehicle: vehicle,
@@ -28,13 +12,6 @@ const plugin = ({ widgets, simulator, vehicle }) => {
         return 10
     })
 
-    return {
-        start_simulation: (time) => {
-            setInterval(async () => {
-                set_speed()
-            }, time);
-        }
-    }
 }
 
 export default plugin;
