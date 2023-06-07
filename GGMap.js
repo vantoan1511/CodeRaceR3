b2865558ed5603876e88e6f019dc01ffd8c61dda
unknown <vantoan1517@gmail.com>
2023-06-07 08:00:44 +0700

    update nho?

--- GGMap.js
+++ GGMap.js
@@ -1,29 +1,63 @@
 import GoogleMapsFromSignal from "./reusable/GoogleMapsFromSignal.js"
 
-const GoogleMapsPlugin = ({widgets, simulator, vehicle}) => {
+const GoogleMapsPlugin = ({ widgets, simulator, vehicle }) => {
+
+    const lat_a = 10.030342
+    const lat_b = 10.030751
+    const lng_a = 105.751816
+    const lng_b = 105.752240
+
+    const init = async () => {
+        simulator("Vehicle.CurrentLocation.Latitude", "get", async () => {
+            return lat_a
+        })
+        simulator("Vehicle.CurrentLocation.Longitude", "get", async () => {
+            return lng_a
+        })
+    }
+
+    init()
+
     widgets.register(
         "GoogleMapDirections",
         GoogleMapsFromSignal(
             [
                 {
-                    "lat": 10.030342,
-                    "lng": 105.751816
+                    "lat": lat_a,
+                    "lng": lng_a
                 },
                 {
-                    "lat": 10.030751,
-                    "lng": 105.752240
+                    "lat": lat_b,
+                    "lng": lng_b
                 },
             ],
             vehicle,
             { iterate: true }
         )
     )
-    simulator("Vehicle.CurrentLocation.Latitude", "get", async () => {
-        return 10.030342
-    })
-    simulator("Vehicle.CurrentLocation.Longitude", "get", async () => {
-        return 105.751816
-    })
+
+    const set_location = async () => {
+        var lat = await vehicle.CurrentLocation.Latitude.get()
+        var lng = await vehicle.CurrentLocation.Longitude.get()
+        if (lat < lat_b && lng < lng_b) {
+            lat += 0.000001
+            lng += 0.000001
+            simulator("Vehicle.CurrentLocation.Latitude", "get", async () => {
+                return lat
+            })
+            simulator("Vehicle.CurrentLocation.Longitude", "get", async () => {
+                return lng
+            })
+        }
+    }
+
+    return {
+        start_simulation: (time) => {
+            setInterval(async () => {
+                set_location()
+            }, time);
+        }
+    }
 }
 
 export default GoogleMapsPlugin
